3ec99019dacc3f44e15c64b814eb66713a03fda6
shvatm <shvatm@post.bgu.ac.il>
2018-11-08 18:59:21 +0200

    init

--- Table.h
+++ Table.h
@@ -41,7 +41,7 @@ public:
 private:
     int capacity;
     bool open;//true when the table is open, false when it's close
-    int tid;
+    int tid=-1;//not sure if its ok that i set it to -1
     std::vector<Customer*> customersList;
     std::vector<OrderPair> orderList; //A list of pairs for each order in a table - (customer_id, Dish)
 };
